e28d4400e8d807fdc2da70c1bb57cd0836f6cea0
Tom <tomc@tomvisions.com>
2024-07-01 18:11:11 -0400

    testing 1.22.4

--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 	echoadapter "github.com/awslabs/aws-lambda-go-api-proxy/echo"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	c "scheduler-api/config"
 	"scheduler-api/db"
 	r "scheduler-api/routes"
+	"strings"
 )
 
 var echoLambda *echoadapter.EchoLambda
@@ -22,9 +26,22 @@ func Handler(ctx context.Context, request events.LambdaFunctionURLRequest) (Resp
 	return Response{Body: "It works!", StatusCode: 200}, nil
 }
 
-func Handler1(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func Handler3(ctx context.Context, request events.LambdaFunctionURLRequest) (Response, error) {
+
+	return Response{Body: "It works!", StatusCode: 200}, nil
+}
+
+func Handler2(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return echoLambda.ProxyWithContext(ctx, req)
 	///	return Response{Body: "It works!", StatusCode: 200}, nil
+}
+func Handler1(ctx context.Context, requests events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	return echoLambda.ProxyWithContext(ctx, requests)
+	///	return Response{Body: "It works!", StatusCode: 200}, nil
+}
+
+type Server struct {
 }
 
 func main() {
@@ -51,13 +68,88 @@ func main() {
 	r.InitRoutes(e)
 	isLambda := os.Getenv("LAMBDA")
 
+	//server.Mount(e)
+
 	if isLambda == "TRUE" {
+		//	lambdaAdapter := &LambdaAdapter{Echo: e}
+		//	lambda.Start(lambdaAdapter.Handler)
 		//		lambdaAdapter := &LambdaAdapter{Echo: e}
 		//		lambda.Start(lambdaAdapter.Handler)
-		lambda.Start(Handler1)
+		lambda.Start(wrapRouter)
 	} else {
 		e.Logger.Fatal(e.Start(":3600"))
 	}
 	//e.Logger.Fatal(e.Start(":3500"))
 	//lambda.Start(Handler)
+}
+
+func wrapRouter(e *echo.Echo) func(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return func(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		body := strings.NewReader(request.Body)
+		req := httptest.NewRequest(request.HTTPMethod, request.Path, body)
+		for k, v := range request.Headers {
+			req.Header.Add(k, v)
+		}
+
+		q := req.URL.Query()
+		for k, v := range request.QueryStringParameters {
+			q.Add(k, v)
+		}
+		req.URL.RawQuery = q.Encode()
+
+		rec := httptest.NewRecorder()
+		e.ServeHTTP(rec, req)
+
+		res := rec.Result()
+		responseBody, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return formatAPIErrorResponse(http.StatusInternalServerError, res.Header, err.Error())
+		}
+
+		return formatAPIResponse(res.StatusCode, res.Header, string(responseBody))
+	}
+}
+
+func formatAPIResponse(statusCode int, headers http.Header, responseData string) (events.APIGatewayProxyResponse, error) {
+	responseHeaders := make(map[string]string)
+
+	responseHeaders["Content-Type"] = "application/json"
+	for key, value := range headers {
+		responseHeaders[key] = ""
+
+		if len(value) > 0 {
+			responseHeaders[key] = value[0]
+		}
+	}
+
+	responseHeaders["Access-Control-Allow-Origin"] = "*"
+	responseHeaders["Access-Control-Allow-Headers"] = "origin,Accept,Authorization,Content-Type"
+
+	return events.APIGatewayProxyResponse{
+		Body:       responseData,
+		Headers:    responseHeaders,
+		StatusCode: statusCode,
+	}, nil
+}
+
+func formatAPIErrorResponse(statusCode int, headers http.Header, err string) (events.APIGatewayProxyResponse, error) {
+	responseHeaders := make(map[string]string)
+
+	responseHeaders["Content-Type"] = "application/json"
+	for key, value := range headers {
+		responseHeaders[key] = ""
+
+		if len(value) > 0 {
+			responseHeaders[key] = value[0]
+		}
+	}
+
+	responseHeaders["Access-Control-Allow-Origin"] = "*"
+	responseHeaders["Access-Control-Allow-Headers"] = "origin,Accept,Authorization,Content-Type"
+
+	return events.APIGatewayProxyResponse{
+		Body:       err,
+		Headers:    responseHeaders,
+		StatusCode: statusCode,
+	}, nil
 }
